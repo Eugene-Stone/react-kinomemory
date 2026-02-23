20394006eab24ac1f3db05b9f7b491dcb95a05b5
Eugene-Stone <eugenestone.work@gmail.com>
2026-02-23 18:06:08 +0200

    Buttons Prev/Next in pagination

--- src/components/FilmsPagination/FilmsPagination.jsx
+++ src/components/FilmsPagination/FilmsPagination.jsx
@@ -1,14 +1,61 @@
 import './FilmsPagination.scss';
 
 export default function FilmsPagination({ currentPage, totalPages, setCurrentPage }) {
+	function handlePrev() {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	}
+
+	function handleNext() {
+		if (currentPage !== totalPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	}
+
+	function getPagination(currentPage, totalPages) {
+		const pages = [];
+
+		const delta = 1; // сколько страниц вокруг текущей
+
+		const rangeStart = Math.max(2, currentPage - delta);
+		const rangeEnd = Math.min(totalPages - 1, currentPage + delta);
+
+		pages.push(1);
+
+		if (rangeStart > 2) {
+			pages.push('...');
+		}
+
+		for (let i = rangeStart; i <= rangeEnd; i++) {
+			pages.push(i);
+		}
+
+		if (rangeEnd < totalPages - 1) {
+			pages.push('...');
+		}
+
+		if (totalPages > 1) {
+			pages.push(totalPages);
+		}
+
+		return pages;
+	}
+
 	return (
 		<div className="pagination-box">
 			<ul className="pagination">
-				<li className="active">
-					<button className="pagination__prev">Prev</button>
+				<li>
+					<button
+						className={
+							currentPage > 1 ? 'pagination__prev' : 'pagination__prev disabled'
+						}
+						onClick={handlePrev}>
+						Prev
+					</button>
 				</li>
 
-				{[...Array(totalPages)].map((_, index) => (
+				{/* {[...Array(totalPages)].map((_, index) => (
 					<li key={index + 1} className={currentPage === index + 1 ? 'active' : ''}>
 						<button
 							className="navigation__link"
@@ -16,10 +63,32 @@ export default function FilmsPagination({ currentPage, totalPages, setCurrentPag
 							{index + 1}
 						</button>
 					</li>
+				))} */}
+
+				{getPagination(currentPage, totalPages).map((page, index) => (
+					<li key={index} className={currentPage === page ? 'active' : ''}>
+						{page === '...' ? (
+							<span>...</span>
+						) : (
+							<button
+								className="navigation__link"
+								onClick={() => setCurrentPage(page)}>
+								{page}
+							</button>
+						)}
+					</li>
 				))}
 
-				<li className="active">
-					<button className="pagination__next">Next</button>
+				<li>
+					<button
+						className={
+							currentPage !== totalPages
+								? 'pagination__next'
+								: 'pagination__next disabled'
+						}
+						onClick={handleNext}>
+						Next
+					</button>
 				</li>
 			</ul>
 		</div>
