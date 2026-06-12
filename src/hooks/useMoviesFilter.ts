df2ac1efd0be031873131996b824de8990ae1145
Eugene-Stone <eugenestone.work@gmail.com>
2026-06-12 16:38:38 +0300

    Update Readme

--- src/hooks/useMoviesFilter.ts
+++ src/hooks/useMoviesFilter.ts
@@ -15,7 +15,7 @@ export function useMoviesFilter(
 
 	// For pagination
 	const [currentPage, setCurrentPage] = useState(1);
-	const limitPerPage = 8;
+	const limitPerPage = 4;
 
 	const [sortType, setSortType] = useState(() => {
 		return localStorage.getItem('sortType') || 'alphabet';
